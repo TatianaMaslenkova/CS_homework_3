8e9f615a578d8216bec49a26fc764b37f3a67a39
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-26 15:44:27 +0300

    task 23 added

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+if(n < 1)
+{
+    Console.WriteLine("Введите положительное число!");
+}
+else
+{
+    while(count <= n)
+    {
+        Console.Write(Math.Pow(count, 3));
+        if(count != n)
+            Console.Write(", ");
+        count++;
+    }
+    
+}
