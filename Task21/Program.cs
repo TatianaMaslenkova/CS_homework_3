4300021a96b6f008cd04650efeb05fdf72a05b90
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-26 15:41:49 +0300

    task 21 added

---
+++ Task21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координаты точки 1 по оси x: ");
+double x1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки 1 по оси y: ");
+double y1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки 1 по оси z: ");
+double z1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки 2 по оси x: ");
+double x2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки 2 по оси y: ");
+double y2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки 2 по оси z: ");
+double z2 = double.Parse(Console.ReadLine()!);
+
+double s = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+
+Console.WriteLine($"Расстояние между точками: {s.ToString("#.00")}");
